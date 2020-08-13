214a4bd424f1008abb63ec139bea0e8208e43886
jimmygranadino <jimmy.granadino@gmail.com>
2020-08-13 11:56:51 -0700

    team page done, resource page done

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import Jimmy from '../assets/jimmy.jpg'
+import Mike from '../assets/mike.jpg'
+import Ann from '../assets/ann.png'
+import Eliott from '../assets/eliott.jpg'
+import Jessie from '../assets/jessie.png'
+import Kevin from '../assets/kevin.jpg'
+import Nick from '../assets/nick.png'
 
 const About = () => {
     return (
@@ -15,7 +21,7 @@ const About = () => {
                 <div class="col-3 mx-auto">
                     <div class="card border-light mb-3">
                         <div class="card-body">
-                            
+                        <img src={Ann} alt="Ann profile" />
                         </div>
                         <div class="card-header">
                             <p class="card-text"><strong>Ann Lee</strong><br/>UX Design</p>
@@ -23,10 +29,28 @@ const About = () => {
                     </div>
                     <div class="card border-light mb-3">
                         <div class="card-body">
-                            
+                        <img src={Kevin} alt="Kevin profile" />
                         </div>
                         <div class="card-header">
                             <p class="card-text"><strong>Kevin Kim</strong><br/>UX Design</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3 mx-auto">
+                    <div class="card border-light mb-3">
+                        <div class="card-body">
+                            <img src={Mike} alt="Mike profile" />
+                        </div>
+                        <div class="card-header">
+                            <p class="card-text"><strong>Mike Dalton</strong><br/>UX Design</p>
+                        </div>
+                    </div>
+                    <div class="card border-light mb-3">
+                        <div class="card-body">
+                            <img src={Jessie} alt="Jessie profile" />
+                        </div>
+                        <div class="card-header">
+                            <p class="card-text"><strong>Jessie Kim</strong><br/>UX Design</p>
                         </div>
                     </div>
                     <div class="card border-light mb-3">
@@ -41,33 +65,15 @@ const About = () => {
                 <div class="col-3 mx-auto">
                     <div class="card border-light mb-3">
                         <div class="card-body">
-                            
+                        <img src={Eliott} alt="Eliott profile" />
                         </div>
                         <div class="card-header">
-                            <p class="card-text"><strong>Mike Dalton</strong><br/>UX Design</p>
+                            <p class="card-text"><strong>Eliott Davidson</strong><br/>Backend Dev</p>
                         </div>
                     </div>
                     <div class="card border-light mb-3">
                         <div class="card-body">
-                            
-                        </div>
-                        <div class="card-header">
-                            <p class="card-text"><strong>Jessie Kim</strong><br/>UX Design</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-3 mx-auto">
-                    <div class="card border-light mb-3">
-                        <div class="card-body">
-                            
-                        </div>
-                        <div class="card-header">
-                            <p class="card-text"><strong>Elliot Davidson</strong><br/>Backend Dev</p>
-                        </div>
-                    </div>
-                    <div class="card border-light mb-3">
-                        <div class="card-body">
-                            
+                            <img src={Nick} alt="Nick profile" />
                         </div>
                         <div class="card-header">
                             <p class="card-text"><strong>Nicholas Foxen</strong><br/>Routing Lead</p>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,13 +8,13 @@ const Home = () => {
                 <div class="col-12 mbrace-bg-2">
                 </div>
             </div>
-            <div class="row justify-content-center my-5" id="fade">
+            <div class="row justify-content-center pb-5 my-5" id="fade">
                 <div class="col-9 px-auto">
                     <p class="lead">Our vision is to create a vibrant community that encourages one another because we believe that words are powerful.</p>
                     <hr class="my-4" />
                     <p>Haven't had a vibe check since quaratine took effect?</p>
                     <p class="lead">
-                        <Link class="btn btn-secondary btn-lg" to="/feeling" role="button">
+                        <Link class="btn btn-lg mbrace-bg-org mbrace-btn" to="/feeling" role="button">
                             Get Started&nbsp;&nbsp;
                             <svg width="2.5vw" height="2.5vw" viewBox="0 0 16 16" class="bi bi-arrow-right-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
