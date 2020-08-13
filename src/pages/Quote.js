3981f05b0fe45bc04ac5092cec8d1a8731e3f64f
jimmygranadino <jimmy.granadino@gmail.com>
2020-08-13 13:15:41 -0700

    quote styling

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -5,7 +5,17 @@ const Quote = (props) => {
     let [ quote, setQuote ] = useState(props)
     return (
         <div>
-            {JSON.stringify(props)}
+            <p></p>
+            <div class="row justify-content-lg-center mx-0" id="fade">
+                <div class="col-12 mbrace-bg-5">
+                </div>
+            </div>
+            <div class="row justify-content-center pb-5 my-5" id="fade">
+                <div class="col-9 px-auto">
+                    <h1 class="display-3">{props.quote}</h1>
+                    <p className="lead text-right">-- {props.author}</p>
+                </div>
+            </div>
         </div>
     )
 }
