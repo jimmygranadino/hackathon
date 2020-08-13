f699fe49ee9997a3793a5e7ff29ad45ef602c535
jimmygranadino <jimmy.granadino@gmail.com>
2020-08-13 01:37:34 -0700

    basic layout almost done, custom css added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Navbar from './pages/Navbar'
+import Feeling from './pages/Feeling'
+import About from './pages/About'
+import Quote from './pages/Quote'
 
 const App = () => {
   return (
     <div className="container px-0">
-      <Navbar />
-      <Home />
+          <Router>
+            <Navbar />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/feeling" component={Feeling} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/quote" component={Quote} />
+          </Router>
     </div>
   );
 }
